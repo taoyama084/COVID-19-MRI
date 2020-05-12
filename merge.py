64a51af375473e8184bff74c153c42b48f2f486b
taoyama084 <64088537+taoyama084@users.noreply.github.com>
2020-05-13 01:30:53 +0900

    merging excel files

---
+++ merge.py
@@ -0,0 +1,23 @@
+#Import packages and set the working directory
+import os
+import glob
+import pandas as pd
+
+# Define csv folder path
+path = 'All_prefectures_xlsx' #os.path.join('Test_csv','Aichi.csv')
+
+#Use glob to match the pattern ‘csv’
+extension = 'xlsx'
+all_filenames = [i for i in glob.glob(os.path.join(path,'*.{}'.format(extension)))]
+
+#df = pd.read_excel(all_filenames[0])
+#print(df.head())
+
+
+#Combine all files in the list and export as CSV
+#combine all files in the list
+combined_xlsx = pd.concat([pd.read_excel(f) for f in all_filenames ])
+#export to csv
+combined_xlsx.to_csv( "combined_csv.csv", index=False, encoding='utf-8-sig')
+
+print("I'm done with this shit")
